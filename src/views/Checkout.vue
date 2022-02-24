b748a8dd3545a99d2533deeb0580e11f6e672172
Sam Wynne <sam.wynne@snapraise.com>
2022-02-24 10:57:00 -0600

    Clean Slate for Demo

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -4,18 +4,15 @@
       <img src="@/assets/Sams.png" alt="Sam's Bottle Shop" />
     </div>
     <div class="flex justify-around bg-blue-300 border text-lg font-semibold">
-      <h3>Time Shopping: {{ timeShopping }}</h3>
+      <h3>Time Shopping:</h3>
     </div>
 
     <h1 class="text-4xl font-bold mt-4">Thank you for your purchase</h1>
-    <Cart class="text-xl font-semibold mt-4" />
-    <h1 class="text-4xl font-bold mt-4">Total Beers: {{ totalCount }}</h1>
-    <h1 class="text-4xl font-bold mt-4">
-      Total Price: {{ formatter.format(totalPrice) }}
-    </h1>
+    <!-- <Cart class="text-xl font-semibold mt-4" /> -->
+    <h1 class="text-4xl font-bold mt-4">Total Beers:</h1>
+    <h1 class="text-4xl font-bold mt-4">Total Price:</h1>
     <button
       class="mb-4 mx-2 bg-red-500 hover:bg-red-300 text-white font-bold py-1 px-2 rounded-md"
-      @click="beerStore.$reset"
     >
       New Shopper
     </button>
@@ -23,14 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import { useBeerStore } from "@/store/useBeerStore";
-import { storeToRefs } from "pinia";
 import Cart from "@/components/Cart.vue";
-
-const beerStore = useBeerStore();
-
-const { totalCount, totalPrice, timeShopping } =
-  storeToRefs(beerStore); /* storeToRefs */
 
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
